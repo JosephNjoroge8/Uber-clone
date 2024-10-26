e548babc29964caa1778bd362d0cdd2a5865655a
JosephNjoroge8 <joem7mach@gmail.com>
2024-10-26 15:34:11 +0300

    "Extracted HomeScreen component and replaced App.js content with it"

---
+++ screens/HomeScreen.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, SafeAreaViewView } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-web'
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaViewView>
+      <Text>welcome to our uber app clone</Text>
+    </SafeAreaViewView>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
